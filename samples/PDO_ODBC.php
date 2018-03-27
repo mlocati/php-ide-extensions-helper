118d26f36c9961d92ba18e578ce2559ecfaf396a
Michele Locati <michele@locati.it>
2018-03-27 15:38:18 +0200

    Start V2

--- samples/PDO_ODBC.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Documentation generated automatically for extension PDO_ODBC v7.1.4 with PHP v7.1.4
-
-// This file doesn't do anything: it's only useful to help editors autocompletion features
-return;
-// No INI entries
-// No constants defined
-// No classes defined
-// No functions defined
